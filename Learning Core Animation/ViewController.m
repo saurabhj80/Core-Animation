7edde833df29f7eb7971a45ae9571a7f5b201317
saurabhj80 <saurabhj80@gmail.com>
2014-12-18 14:12:36 -0800

    learning my way through

--- Learning Core Animation/ViewController.m
+++ Learning Core Animation/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *layerview;
+@property (nonatomic, strong)  UIView *coneView;
+
 @end
 
 @implementation ViewController
@@ -17,11 +20,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.coneView = [[UIView alloc] init];
+    
+    self.coneView.frame = CGRectMake(100, 100, 100, 100);
+    
+    [self.layerview addSubview:self.coneView];
+    
+    UIImage* image = [UIImage imageNamed:@"logo.png"];
+    
+    // Setting Background Color
+    [self setBackgroungColorToLayer:[UIColor blackColor] toLayer:self.coneView.layer];
+    
+    
+    // Setting Image To layer
+    [self setImageToLayer:image toLayer:self.coneView.layer];
+    
+    
+    // Setting a strechable image
+    [self addStrechableImage:image withContenCenter:CGRectMake(0.25, 0.25, 0.75, 0.75) toViewLayer:self.coneView.layer];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Helper Methods
+
+- (void) addStrechableImage:(UIImage *)image withContenCenter:(CGRect)rect toViewLayer:(CALayer*)layer
+{
+    layer.contents = (__bridge id)image.CGImage;
+    layer.contentsCenter = rect;
+    
+}
+
+- (void) setImageToLayer:(UIImage*)image toLayer:(CALayer *)layer
+{
+    layer.contents = (__bridge id)image.CGImage;
+    
+    
+}
+
+- (void) setBackgroungColorToLayer:(UIColor*)color toLayer:(CALayer *)layer
+{
+    
+    layer.backgroundColor = color.CGColor;
 }
 
 @end
